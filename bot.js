f15e95593e6f3ed7d6bd1b5a10af3bb3b3175704
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-14 20:25:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -394,14 +394,14 @@ client.on("message", message => {
             	.query({ limit: 800 });
         	const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
         	if (!allowed.length) return message.channel.send('Allah is angry, so no funny for you');
-        	const randomnumber = Math.floor(Math.random() * allowed.length)
+        	const randomnumber = Math.floor(Math.random() * allowed.length);
         	const embed = new Discord.RichEmbed()
         	.setColor(0x00A2E8)
         	.setTitle(allowed[randomnumber].data.title)
         	.setDescription("Posted by: " + allowed[randomnumber].data.author)
         	.setImage(allowed[randomnumber].data.url)
         	.addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-        	.setFooter("Memes provided by r/dankmemes")
+        	.setFooter("funny provided by allah");
         	message.channel.send(embed);
     		/* catch (err) {
         	return console.log("oops lol");
