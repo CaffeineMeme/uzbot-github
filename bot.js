f4cb64fede4ac394d27cae6a18352b4b2b757a6b
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-16 17:58:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -422,8 +422,9 @@ client.on("message", message => {
 		if(command === 'arabfunny')
 		{
 			fetch('http://www.reddit.com/r/arabfunny/new.json?sort=new', { method: 'POST', body: 'a=1', limit: 800})
-    			.then(res => res.json()
-			.then(json => message.channel.send(json)));
+    			.then(res => res.json())
+			.then(json => message.channel.send(json))
+			.then(json => console.log(json)));
 		}
 		if(command === 'funnymeter')
 		{	
