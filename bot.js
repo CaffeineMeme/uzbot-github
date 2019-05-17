d3bd820599aac9dd4eafbeb1e805d8a4ba5959c2
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-17 08:39:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -427,16 +427,15 @@ client.on("message", message => {
             .query({ limit: 800 });
         const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
         if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-        const randomnumber = Math.floor(Math.random() * allowed.length)
+        const randomnumber = Math.floor(Math.random() * allowed.length);
         const embed = new Discord.RichEmbed()
         .setColor(0x00A2E8)
         .setTitle(allowed[randomnumber].data.title)
         .setDescription("Posted by: " + allowed[randomnumber].data.author)
         .setImage(allowed[randomnumber].data.url)
         .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-        .setFooter("Memes provided by r/dankmemes")
-        message.channel.send(embed)
-    	}
+        .setFooter("Memes provided by r/dankmemes");
+        message.channel.send(embed);
 }
 		}
 		if(command === 'funnymeter')
