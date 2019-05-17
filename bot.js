b92d26b6f4ee339203cea2700fed7295c425370e
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-17 08:34:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -421,13 +421,26 @@ client.on("message", message => {
 		}
 		if(command === 'arabfunny')
 		{
-			const { body } = fetch({method: 'http://www.reddit.com/r/arabfunny/new.json?sort=new', limit: 800})
-    			//.then(res => res.json())
-			//.then(json => message.channel.send(json))
-			//.then(json => console.log(json));
-			const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-        		if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-        		const randomnumber = Math.floor(Math.random() * allowed.length)
+			exports.run = async (client, message, args) => {
+    try {
+        const { body } = await snek
+            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
+            .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        const embed = new Discord.RichEmbed()
+        .setColor(0x00A2E8)
+        .setTitle(allowed[randomnumber].data.title)
+        .setDescription("Posted by: " + allowed[randomnumber].data.author)
+        .setImage(allowed[randomnumber].data.url)
+        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+        .setFooter("Memes provided by r/dankmemes")
+        message.channel.send(embed)
+    } catch (err) {
+        return console.log(err);
+    }
+}
 		}
 		if(command === 'funnymeter')
 		{	
