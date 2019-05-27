5dd22138c5fc4f0325c4f4d9fc469420b1dffdea
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-27 12:02:54 -0500

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
         
         if(args[0] === null || args[0] === undefined){
     	message.channel.send('You have a balance of лв`' + bal + '`' + som);
-        }else if(args[0] === message.mentions.users.first())
+        }else if(args[0] != null || args[0] != undefined)
         {
                 let user = message.mentions.users.first().id;
                 let otherbal = db.fetch(`money_${user.id}`)
