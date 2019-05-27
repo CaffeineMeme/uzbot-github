dd043b4114e9c470427da92aa5f4d1c6aea7b380
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-27 17:30:58 -0500

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -2,15 +2,17 @@ const db = require('quick.db');
 const Discord = require('discord.js');
 
 var canWork = true;
-var timeLeft;
+var timeLeft = db.fetch(`timeLeft_${message.author.id}`);
 var timeRemaining;
+
+var timer = setInterval(  () => {timeLeft--;}, 1000);
 
 exports.run = async (client, message, args, config) => {
     
     if(timeLeft <= 0)
         {
             clearTimeout();
-            clearInterval();
+            clearInterval(timer);
             canWork = true;
             timeLeft = 0;
         }
@@ -26,14 +28,13 @@ if(canWork === true){
         .setDescription(`${message.author}, you prayed to Allah and he gave you ${amount}$`)
         .setColor("RANDOM");
         
-        timeLeft = 20;
+        timeLeft = db.add(`timeLeft_${message.author.id}`, 20);
         timeRemaining = 20;
         message.channel.send(embed);
         db.add(`totalMoney_${message.author.id}`, amount);
         db.add(`money_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        setInterval(  () => {timeLeft--;}, 1000);
     } else if(args[0] == 'troll') {
         let amount = Math.floor(Math.random() * 350) + 1; // 1-500 random number. whatever you'd like
 
@@ -42,14 +43,13 @@ if(canWork === true){
         .setDescription(`${message.author}, you trolled some gay niggas, and stole ${amount}$ worth of JahCoin.`)
         .setColor("RANDOM");
         
-        timeLeft = 35;
+        timeLeft = db.add(`timeLeft_${message.author.id}`, 35);
         timeRemaining = 35;
         message.channel.send(embed);
         db.add(`totalMoney_${message.author.id}`, amount);
         db.add(`money_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        setInterval(  () => {timeLeft--;}, 1000);
     } else if(args[0] == 'terrorist') {
         let amount = Math.floor(Math.random() * 600) + 1; // 1-500 random number. change to whatever you'd like
 
@@ -59,13 +59,13 @@ if(canWork === true){
         .setColor("RANDOM");
         
     
-        timeLeft = 50;
+        timeLeft = db.add(`timeLeft_${message.author.id}`, 50);
+        timeRemaining = 35;
         message.channel.send(embed);
         db.add(`money_${message.author.id}`, amount);
         db.add(`totalMoney_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        setInterval(  () => {timeLeft--;}, 1000);
     }
     }else{
         message.channel.send("Yo hold on you've only got " + timeLeft + " seconds before you can work again");
