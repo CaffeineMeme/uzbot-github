b9da78f673da945b2c392571c59e9b61e0f3901c
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-27 16:35:51 -0500

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -6,24 +6,26 @@ var timeLeft;
 
 exports.run = async (client, message, args, config) => {
 if(canWork === true){
-    clearInterval();
     clearTimeout();
     if (args[0] == 'preacher') {
 
-        let amount = Math.floor(Math.random() * 100) + 1; // 1-500 random number. whatever you'd like
+        let amount = Math.floor(Math.random() * 150) + 1; // 1-500 random number. whatever you'd like
 
         let embed = new Discord.RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you prayed to Allah and he gave you ${amount}$`)
         .setColor("RANDOM");
         
-        timeLeft = 10;
+        timeLeft = 20;
         message.channel.send(embed);
         db.add(`totalMoney_${message.author.id}`, amount);
         db.add(`money_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        setInterval(  () => {timeLeft--;}, 1000);
+        for(i = 0; i >= timeLeft)
+        {
+            setTimeout(  () => {  i++;   timeLeft--; }, 1000);
+        }
     } else if(args[0] == 'troll') {
         let amount = Math.floor(Math.random() * 260) + 1; // 1-500 random number. whatever you'd like
 
@@ -32,13 +34,16 @@ if(canWork === true){
         .setDescription(`${message.author}, you trolled some gay niggas, and stole ${amount}$ worth of JahCoin.`)
         .setColor("RANDOM");
         
-        timeLeft = 15;
+        timeLeft = 35;
         message.channel.send(embed);
         db.add(`totalMoney_${message.author.id}`, amount);
         db.add(`money_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        setInterval(  () => {timeLeft--;}, 1000);
+        for(i = 0; i >= timeLeft)
+        {
+            setTimeout(  () => {  i++;   timeLeft--; }, 1000);
+        }
     } else if(args[0] == 'terrorist') {
         let amount = Math.floor(Math.random() * 420) + 1; // 1-500 random number. change to whatever you'd like
 
@@ -48,15 +53,18 @@ if(canWork === true){
         .setColor("RANDOM");
         
     
-        timeLeft = 20;
+        timeLeft = 50;
         message.channel.send(embed);
         db.add(`money_${message.author.id}`, amount);
         db.add(`totalMoney_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        setInterval(  () => {timeLeft--;}, 1000);
+        for(i = 0; i >= timeLeft)
+        {
+            setTimeout(  () => {  i++;   timeLeft--; }, 1000);
+        }
     }
     }else{
-        message.channel.send("Yo hold on you've only got " + timeLeft + " before you can work again");
+        message.channel.send("Yo hold on you've only got " + timeLeft + " seconds before you can work again");
     }
 }
