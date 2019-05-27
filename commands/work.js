36185c8a3beea957a4134732419ca6f1ebaadce6
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-27 10:48:02 -0500

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -1,5 +1,5 @@
-const db = require('quick.db')
-const Discord = require('discord.js')
+const db = require('quick.db');
+const Discord = require('discord.js');
 
 exports.run = async (client, message, args, config) => {
 
@@ -10,31 +10,32 @@ exports.run = async (client, message, args, config) => {
         let embed = new Discord.RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you prayed to Allah and he gave you ${amount}$`)
-        .setColor("RANDOM")
+        .setColor("RANDOM");
         
     
-        message.channel.send(embed)
-        db.add(`money_${message.author.id}`, amount)
+        message.channel.send(embed);
+        db.add(`money_${message.author.id}`, amount);
     } else if(args[0] == 'troll') {
         let amount = Math.floor(Math.random() * 260) + 1; // 1-500 random number. whatever you'd like
 
         let embed = new Discord.RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you trolled some gay niggas, and stole ${amount}$ worth of JahCoin.`)
-        .setColor("RANDOM")
+        .setColor("RANDOM");
         
     
-        message.channel.send(embed)
-        db.add(`money_${message.author.id}`, amount)
+        message.channel.send(embed);
+        db.add(`money_${message.author.id}`, amount);
     } else if(args[0] == 'terrorist') {
         let amount = Math.floor(Math.random() * 420) + 1; // 1-500 random number. change to whatever you'd like
 
         let embed = new Discord.RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you bombed a tower and stole ${amount}$ in flaming money`)
-        .setColor("RANDOM")
+        .setColor("RANDOM");
         
     
-        message.channel.send(embed)
-        db.add(`money_${message.author.id}`, amount)
+        message.channel.send(embed);
+        db.add(`money_${message.author.id}`, amount);
     }
+}
