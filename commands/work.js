9781f27d1fa1fa2a90360249e7731b833e48c318
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-27 16:54:34 -0500

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -22,7 +22,7 @@ if(canWork === true){
         db.add(`money_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        for(i = 0; i >= timeLeft;)
+        for(i = 0; i <= timeLeft;)
         {
             setTimeout(  () => {  i++;   timeLeft--; }, 1000);
         }
@@ -40,7 +40,7 @@ if(canWork === true){
         db.add(`money_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        for(i = 0; i >= timeLeft;)
+        for(i = 0; i <= timeLeft;)
         {
             setTimeout(  () => {  i++;   timeLeft--; }, 1000);
         }
@@ -59,9 +59,9 @@ if(canWork === true){
         db.add(`totalMoney_${message.author.id}`, amount);
         canWork = false;
         setTimeout(  () => {    canWork = true;  },  timeLeft * 1000);
-        for(i = 0; i >= timeLeft;)
+        for(i = 0; i <= timeLeft;)
         {
-            setTimeout(  () => {  i++;   timeLeft--; }, 1000);
+            setTimeout(  () => {  i++;  timeLeft--; }, 1000);
         }
     }
     }else{
