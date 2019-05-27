4410e753e46a6c16a57eeba3e837c47980a8484f
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-27 16:09:06 -0500

    Update employ.js

--- commands/employ.js
+++ commands/employ.js
@@ -7,9 +7,9 @@ exports.run = async (client, message, args, config) => {
   if(args[0] == null || args[0] == undefined)
   {
     let level = db.fetch(`level_${message.author.id}`);
-    jobtitle = args.join(" ");
-    if(args[0] == "preacher" || args[0] == "street merchant") || ((args[0] == "hijab maker" || args[0] == "executioner") && level == 1) || ((args[0] == "tech support" || args[0] == "saudi comedian") && level == 2) || ((args[0] == "quran printer" || args[0] == "ISIS manager") && level == 3) || ((args[0] == "terrorist" || args[0] == "shitting street supervisor") && level == 4)){
-    db.add(job_${message.author.id}`, jobTitle); 
+    jobTitle = args.join(" ");
+    if((jobTitle == "preacher" || jobTitle == "street merchant")) || ((jobTitle == "hijab maker" || jobTitle == "executioner") && level == 1) || ((jobTitle == "tech support" || == "saudi comedian") && level == 2) || ((jobTitle == "quran printer" || jobTitle == "ISIS manager") && level == 3) || ((jobTitle == "terrorist" || jobTitle == "shitting street supervisor") && level == 4)){
+    db.add(`job_${message.author.id}`, jobTitle); 
     }else{
       message.channel.send('you are too infidel, level up more first');
     }
