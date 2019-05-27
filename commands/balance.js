8540888bbce5c1a5f342ad064b33ab5a0f654a57
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-27 10:38:50 -0500

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const db = require('quick.db');
-const som = client.emojis.find(emoji => emoji.name === "som");
 
 module.exports.run = async (client, message, args) => {
+        let som = client.emojis.find(emoji => emoji.name === "som");
         let bal = db.fetch(`money_${message.guild.id}_${message.author.id}`)
 
    	if (bal === null) bal = 0;
 
-    	message.channel.send('You have a balance of `' + bal + '`');
+    	message.channel.send('You have a balance of лв`' + bal + '`' + som);
 }
