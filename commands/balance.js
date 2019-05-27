e3c9bcb3c85b4556f3c6cd4211d570e9cafe7bdc
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-27 11:52:10 -0500

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -4,9 +4,27 @@ const db = require('quick.db');
 
 module.exports.run = async (client, message, args) => {
         let som = client.emojis.find(emoji => emoji.name === "som");
-        let bal = db.fetch(`money_${message.author.id}`)
+        let lol = client.emojis.find(emoji => emoji.name === "trollsmile");
+        let bal = db.fetch(`money_${message.author.id}`);
 
    	if (bal === null) bal = 0;
-
+        
+        if(args[1] === null || args[1] === undefined){
     	message.channel.send('You have a balance of лв`' + bal + '`' + som);
+        }else if(args[1] === message.mentions.users.first())
+        {
+                let user = message.mentions.users.first().id;
+                let otherbal = db.fetch(`money_${user.id}`)
+                message.channel.send(user + " has a balance of лв`' + otherbal + '`' + som);
+                if(otherbal > bal)
+                {
+                message.channel.send("They're richer than you " + lol); 
+                }else if(otherbal > bal)
+                {
+                message.channel.send("They're poorer than you " + lol); 
+                }if(otherbal === bal)
+                {
+                message.channel.send("You are equally poor " + lol); 
+                }
+        }
 }
