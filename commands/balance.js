49030313e882145e147093060e13de690078322e
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-28 07:41:46 -0500

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -4,7 +4,6 @@ const db = require('quick.db');
 
 module.exports.run = async (client, message, args) => {
         let som = client.emojis.find(emoji => emoji.name === "som");
-        let lol = client.emojis.find(emoji => emoji.name === "trollsmile");
         let bal = db.fetch(`money_${message.author.id}`);
 
    	if (bal === null) bal = 0;
@@ -16,15 +15,19 @@ module.exports.run = async (client, message, args) => {
                 let user = message.mentions.users.first();
                 let otherbal = db.fetch(`money_${user.id}`)
                 if (otherbal === null) otherbal = 0;
+                message.channel.send('You have a balance of лв`' + bal + '`' + som);
                 message.channel.send(user + ' has a balance of лв`' + otherbal + '`' + som);
                 if(otherbal > bal)
                 {
+                let lol = client.emojis.find(emoji => emoji.name === "danbull");
                 message.channel.send("They're richer than you " + lol); 
                 }else if(otherbal < bal)
                 {
+                let lol = client.emojis.find(emoji => emoji.name === "blacktroll");
                 message.channel.send("They're poorer than you " + lol); 
                 }if(otherbal === bal)
                 {
+                let lol = client.emojis.find(emoji => emoji.name === "trollsmile");
                 message.channel.send("You are equally poor " + lol); 
                 }
         }
