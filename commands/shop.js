e2fe9cf5a328ad9144fd9f483564724058f03bd7
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-05-29 08:49:53 -0500

    Create shop.js

---
+++ commands/shop.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+var shopList = [];
+
+module.exports.run = async (bot, message, args) => {
+
+	shopList = db.get(`shopItem${message.guild.id}`);
+	console.log(shopList);
+	
+	let embed = Discord.RichEmbed()
+	.setTitle("Uzbekistan Funny Shop lole")
+	.addField("shopList[0]");
+	
+	message.channel.send(embed);
+
+}
