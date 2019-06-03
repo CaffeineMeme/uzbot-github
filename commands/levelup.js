c158b07b1c0ec9cb4dbe684a512b737df8ff900e
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-06-03 07:05:17 -0500

    Update levelup.js

--- commands/levelup.js
+++ commands/levelup.js
@@ -13,14 +13,14 @@ module.exports.run = async (bot, message, args) =>{
     if (!message.member.hasPermission('ADMINISTRATOR')) {
         return message.reply('u cant do that nigga');
     }else if (message.member.hasPermission('ADMINISTRATOR') || message.author.id == "287794457594822657"){
-    var levelUpGoal =  db.get(`levelUpGoal_${message.author.id}`);
-    var levelUpExpo = db.get(`levelUpExp_${message.author.id}`);
+    var levelUpGoal =  db.get(`levelUpGoal_${user.id}`);
+    var levelUpExpo = db.get(`levelUpExp_${user.id}`);
     
-    db.add(`levelUpExp_${message.author.id}`, .0125);
-    db.set(`levelUpGoal_${message.author.id}`, (Math.ceil((((levelUpGoal * 2.5 ^ levelUpExpo) - (levelUpGoal * 0.2) + 1)/10) * 10)));
+    db.add(`levelUpExp_${user.id}`, .0125);
+    db.set(`levelUpGoal_${user.id}`, (Math.ceil((((levelUpGoal * 2.5 ^ levelUpExpo) - (levelUpGoal * 0.2) + 1)/10) * 10)));
     
     message.channel.send("congrats an admin or someone important and cool leveled you up");
-    db.add(`level_${message.author.id}`, 1);
+    db.add(`level_${user.id}`, 1);
     }
     
 }
