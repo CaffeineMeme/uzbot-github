e51fc71df1d6cfaabc351153b00b8d1d4c0530be
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-06-29 21:26:37 -0500

    Update levelup.js

--- commands/levelup.js
+++ commands/levelup.js
@@ -19,11 +19,10 @@ module.exports.run = async (bot, message, args) =>{
     var levelUpExpo = db.get(`levelUpExp_${user.id}`);
     
     db.add(`levelUpExp_${user.id}`, .0125);
-    db.add(`levelUpGoal_${user.id}`, (Math.ceil((((levelUpGoal * 2.5 ^ levelUpExpo)/10) * 10))));
+    levelUpGoal = levelUpGoal + (Math.ceil((((levelUpGoal * 2.35 ^ levelUpExpo)/10) * 10))));
     
     message.channel.send("congrats an admin or someone important and cool leveled you up");
-    goal = db.get(`levelUpGoal_${user.id}`);
-    console.log(goal);
+    console.log(levelUpGoal);
     console.log(user.id);
     db.add(`level_${user.id}`, 1);
     }
