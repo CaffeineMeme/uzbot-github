76196dd2af2ee3c1145e23782bc2e9689917c870
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-06-30 17:57:36 -0500

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -39,7 +39,9 @@ exports.run = async (client, message, args, config) => {
         db.set(`jobWait_${message.author.id}`, 15);
         }
         successMsg = "You made allah happy so he gave you ";
+            successMsg2 = "";
         failSmg = "You made allah fuckin angry so he pissed on you and stole ";
+            failSmg2 = " in reparations";
         }
         else if (job == 'street merchant') {
         earnMax = 275;
@@ -50,7 +52,9 @@ exports.run = async (client, message, args, config) => {
         db.set(`jobWait_${message.author.id}`, 20);
         }
         successMsg = "You sold your garbage to dumbasses and got ";
-        failSmg = "Some niggas stole your shit and about ";
+            successMsg2 = "";
+        failSmg = "Some niggas stole your shit and you lost about ";
+            failSmg2 = " and like half your stock";
         }
         else if (job == 'hijab maker') {
         earnMax = 360;
@@ -61,7 +65,9 @@ exports.run = async (client, message, args, config) => {
          db.set(`jobWait_${message.author.id}`, 30);
             }
             successMsg = "The hijab covers her face well, so you get ";
+            successMsg2 = "";
         failSmg = "The hijab falls apart, and your client is beat to paralysis by the police, so she steals ";
+            failSmg2 = " from you";
         }
         else if (job == 'executioner') {
         earnMax = 250;
@@ -72,7 +78,9 @@ exports.run = async (client, message, args, config) => {
        db.set(`jobWait_${message.author.id}`, 35);
             }
         successMsg = "The head flies and the prince throws you ";
-        failSmg = "You chop off the retards nose, he runs away and from you steals ";
+            successMsg2 = "";
+        failSmg = "You chop off the retards nose, he steals ";
+        failSmg2 = " and runs away.";
         }
         else if (job == 'tech support') {
         earnMax = 650;
@@ -83,7 +91,9 @@ exports.run = async (client, message, args, config) => {
             {db.set(`jobWait_${message.author.id}`, 25);
             }
         successMsg = "You get the dumdum's bank account and grab a nice ";
-        failSmg = "Your computer blows up and repairs cost ";
+            successMsg2 = "";
+        failSmg = "Your computer blows up and you spend ";
+        failSmg = " to repair it";
         }
         else if (job == 'saudi comedian') {
         earnMax = 920;
@@ -94,8 +104,102 @@ exports.run = async (client, message, args, config) => {
         cooldownTime = db.set(`jobWait_${message.author.id}`, 55);
             }
         successMsg = "You make a funny and get ";
+            successMsg2 = "";
         failSmg = "You make the audience mad. They stone you and medical bills cost ";
+            failSmg2 = "";
         }
+        else if (job == 'kaaba repairman') {
+        earnMax = 600;
+        earnMin = 270;
+        failMin = 0;
+        failMax = 18;
+            if(canWork = true) {
+        cooldownTime = db.set(`jobWait_${message.author.id}`, 30);
+            }
+        successMsg = "You fix the kaaba very good and Allah gives you ";
+            successMsg2 = "";
+        failSmg = "You chip off a block killing a diciple and their hospital bill costs ";
+            failSmg2 = "";
+        }
+        else if (job == 'quran printer') {
+        earnMax = 810;
+        earnMin = 370;
+        failMin = 0;
+        failMax = 22;
+            if(canWork = true) {
+        cooldownTime = db.set(`jobWait_${message.author.id}`, 25);
+            }
+        successMsg = "You print a nice quran and get ";
+             successMsg2 = "";
+        failSmg = "Your dumb ass printed it all in English and you need to reprint, and you spend ";
+        failSmg2 = " to make arab subtitles";
+        }
+        else if (job == 'ISIS manager') {
+        earnMax = 600;
+        earnMin = 1160;
+        failMin = 0;
+        failMax = 26;
+            if(canWork = true) {
+        cooldownTime = db.set(`jobWait_${message.author.id}`, 55);
+            }
+        successMsg = "You send your best men to kill the infidels and they loot ";
+        successMsg2 = " from the pigs and give it to you";
+        failSmg = "Some of your best men are killed and you have to pay a lowly peasant ";
+        failSmg2 = " to fetch their AK47s, after all, you can't let them go to waste";
+        }
+        else if (job == 'terrorist') {
+        earnMax = 600;
+        earnMin = 1160;
+        failMin = 0;
+        failMax = 26;
+            if(canWork = true) {
+        cooldownTime = db.set(`jobWait_${message.author.id}`, 75);
+            }
+        successMsg = "You send your best men to kill the infidels and they loot ";
+        successMsg2 = " from the pigs and give it to you";
+        failSmg = "Some of your best men are killed and you have to pay a lowly peasant ";
+        failSmg2 = " to fetch their AK47s, after all, you can't let them go to waste";
+        }
+        else if (job == 'street shitting supervisor') {
+        earnMax = 570;
+        earnMin = 990;
+        failMin = 0;
+        failMax = 14;
+            if(canWork = true) {
+        cooldownTime = db.set(`jobWait_${message.author.id}`, 35);
+            }
+        successMsg = "You make organized poopoos on the street, the citizens give you ";
+        successMsg2 = " for your coordination skills";
+        failSmg = "You have make such a bad poopoo coordination that they leave and poo in the loo, costing you ";
+        failSmg2 = "";
+        }
+        else if (job == 'gay stripper') {
+        earnMax = 2400;
+        earnMin = 4800;
+        failMin = 0;
+        failMax = 69;
+            if(canWork = true) {
+        cooldownTime = db.set(`jobWait_${message.author.id}`, 120);
+            }
+        successMsg = "You strip like a mega gay and the fags toss you ";
+        successMsg2 = " which you stuff in your ass";
+        failSmg = "The police bust the bar and shoot everyone in the bar and you get shot in the cock. Repairs cost ";
+        failSmg2 = " and they overcharge because you're gay";
+        }
+             else if (job == 'booze smuggler') {
+        earnMax = 1230;
+        earnMin = 1990;
+        failMin = 0;
+        failMax = 30;
+            if(canWork = true) {
+        cooldownTime = db.set(`jobWait_${message.author.id}`, 40);
+            }
+        successMsg = "You sell booze to the infidel and make ";
+        successMsg2 = " before you ask Allah for forgiveness";
+        failSmg = "The saudi prince sees you and steals all of your booze for himself. Oh yeah and he also steals ";
+        failSmg2 = " for recoupment fines.";
+        }
+            
         
         
         let fail = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
@@ -105,7 +209,7 @@ exports.run = async (client, message, args, config) => {
         {
             let embed = new Discord.RichEmbed()
             .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
-            .setDescription(`${message.author} , ${failSmg} ${amount}${som}`)
+            .setDescription(`${message.author} , ${failSmg}${amount}${som}${failSmg2}`)
             .setColor("RANDOM");
             message.channel.send(embed);
              db.add(`money_${message.author.id}`, (amount * -1));
@@ -115,7 +219,7 @@ exports.run = async (client, message, args, config) => {
         {
             let embed = new Discord.RichEmbed()
             .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
-            .setDescription(`${message.author} , ${successMsg} ${amount}${som}`)
+            .setDescription(`${message.author} , ${successMsg}${amount}${som}${successMsg2}`)
             .setColor("RANDOM");
             message.channel.send(embed);
             db.add(`money_${message.author.id}`, (amount));
