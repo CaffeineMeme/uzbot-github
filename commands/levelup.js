428a54bb99ea75bed30bbeb2552b53d5f75812eb
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-06-30 21:48:36 -0500

    Update levelup.js

--- commands/levelup.js
+++ commands/levelup.js
@@ -17,16 +17,18 @@ module.exports.run = async (bot, message, args) =>{
     }else if (message.member.hasPermission('ADMINISTRATOR') || message.author.id == "287794457594822657"){
     let levelUpGoal =  db.fetch(`levelUpGoal_${user.id}`);
     let levelUpExpo = db.fetch(`levelUpExp_${user.id}`);
-    let amountToAdd = (Math.ceil(((((levelUpGoal * 1.35) ^ levelUpExpo)/10) * 10) - (levelUpGoal * 0.4)));
+    let amountToAdd = (Math.ceil(((((levelUpGoal * 1.3) ^ levelUpExpo)/10) * 10) - (levelUpGoal * 0.45)));
     
     db.add(`levelUpExp_${user.id}`, .0035);
         
     let level =  db.get(`level_${user.id}`);
     let newGoal = 0
     if(level == 0){
-    newGoal = Math.round((levelUpGoal * 1.2) + amountToAdd);
-    }else{
+    newGoal = Math.round((levelUpGoal * 1.3) + amountToAdd);
+    }else if(level > 0 && level < 6){
     newGoal = Math.round((levelUpGoal * 0.875) + amountToAdd);   
+    }else if(level > 5){
+    newGoal = Math.round((levelUpGoal * 0.6) + (amountToAdd * 0.8));   
     }
     
     message.channel.send("congrats an admin or someone important and cool leveled you up");
