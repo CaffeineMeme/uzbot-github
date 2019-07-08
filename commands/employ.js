ffa4f23b8b9fe9d33a983fd9e9cb706022642dc3
Memel0rdCoffeeCup <acoffeecup0308@gmail.com>
2019-07-08 13:48:12 -0500

    Update employ.js

--- commands/employ.js
+++ commands/employ.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 
 var jobTitle;
 var canApply;
+var limit = 4
 
 exports.run = async (client, message, args, config) => {
   let cooldown = db.fetch(`jobCooldown_${message.author.id}`);
@@ -11,7 +12,7 @@ exports.run = async (client, message, args, config) => {
   if(canApply == false){
     message.channel.send('work more and then you can quit bro') 
   }else {
-  if(args[0] != null || args[0] != undefined)
+  if((args[0] != null || args[0] != undefined) && isNaN(args[0]))
   {
     if(args[0] == "quit"){
       message.channel.send('ok bro u quit now find a new job lmao');
@@ -30,17 +31,19 @@ exports.run = async (client, message, args, config) => {
      setTimeout(  () => {    db.set(`canApply_${message.author.id}`, true);  },  cooldown * 1000);
     }
     }
-  }else{
-    message.channel.send('choose a job first');
+  }else if((args[0] != null || args[0] != undefined) && !isNaN(args[0]) && (args[0] =< limit && args[0] > 0)){
+    message.channel.send();
     let embed = new Discord.RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
-        .setDescription(`List of available jobs`, `FORMAT EXAMPLE: -uz employ preacher`)
+        .setDescription('Page ' + args[0] + " of Job Listings", "Page " + args[0] + "of " + limit)
+        if(args[0] == 1){
         .addField("Preacher", "Praise allah to pay off allah" + "\n" + "Payment: 60 - 190" + som + "\n" + "Fail Rate: 10%" + "\n" +  "Level Requirement: 0+", true)
         .addField("Street Merchant", "Sell your trash to dumb niggas" + "\n" + "Payment: 100 - 275" + som + "\n" + "Fail Rate: 17%" + "\n" + "Level Requirement: 0+", true)
         .addField("Hijab Maker", "Cover em up" + "\n" + "Payment: 150 - 360" + som + "\n" + "Fail Rate: 12%" + "\n" + "Level Requirement: 1+", true)
         .addField("Executioner", "Fun job for everyone" + "\n" + "Payment: 100 - 250" + som + "\n" + "Fail Rate: 6%" + "\n" + "Level Requirement: 1+", true)
         .addField("Tech Support", "Hello this is Rajeesh of Microsoft, how may I assist you?" + "\n" + "Payment: 300 - 650" + som + "\n" + "Fail Rate: 15%" + "\n" + "Level Requirement: 2+", true)
         .addField("Saudi Comedian", "Make Allah laugh" + "\n" + "Payment: 500 - 920" + som + "\n" + "Fail Rate: 23%" + "\n" + "Level Requirement: 2+", true)
+        }else if(args[0] == 2){
         .addField("Kaaba Repairman", "Fix the holy block please" + "\n" + "Payment: 270 - 600" + som + "\n" + "Fail Rate: 18%" + "\n" + "Level Requirement: 2+", true)
         .addField("Quran Printer", "Print holy books for Allah" + "\n" + "Payment: 370 - 810" + som + "\n" + "Fail Rate: 16%" + "\n" + "Level Requirement: 3+", true)
         .addField("ISIS Manager", "Show the infidels who's the boss" + "\n" + "Payment: 660 - 1060" + som + "\n" + "Fail Rate: 26%" + "\n" + "Level Requirement: 3+", true)
@@ -48,26 +51,13 @@ exports.run = async (client, message, args, config) => {
         .addField("Shitting Street Supervisor", "Pajeet say no to Poo2Loo" + "\n" + "Payment: 570 - 990" + som + "\n" + "Fail Rate: 14%" + "\n" + "Level Requirement: 4+", true)
         .addField("Gay Stripper", "Appeal to the gay" + "\n" + "Payment: 2400 - 4800" + som + "\n" + "Fail Rate: 42%" + "\n" + "Level Requirement: 5+", true)
         .addField("Booze Smuggler", "Pay for drink and drive please" + "\n" + "Payment: 1230 - 1940" + som + "\n" + "Fail Rate: 28%" + "\n" + "Level Requirement: 4+", true)
-    .setColor("RANDOM");
-  message.channel.send(embed);
-    db.set(`canApply_${message.author.id}`, true);
+        }
+        .setColor("RANDOM");
+        message.channel.send(embed);
+        db.set(`canApply_${message.author.id}`, true);
 
-const collector = message.createReactionCollector((reaction, user) => 
-    user.id === message.author.id &&
-    reaction.emoji.name === "◀" ||
-    reaction.emoji.name === "▶" ||
-    reaction.emoji.name === "❌"
-).once("collect", reaction => {
-    const chosen = reaction.emoji.name;
-    if(chosen === "◀"){
-        console.log("workinglol");
-    }else if(chosen === "▶"){
-        console.log("lolworking");
-    }else{
-        console.log("fart");
-    }
-    collector.stop();
-});
+}else{
+  message.channel.send("you didn't put an valid argument dumbass" + "\n" + "type in a page valid page number to see job listings (ex. -uz employ 2)" + "\n" + "or type in the job you want (ex. -uz employ preacher)");
 }
 }
 }
